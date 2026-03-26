0e86fcd1a907042ea8f71d86d258477022bdf9c9
bryan96ch <bryan96ch@hotmail.com>
2026-03-25 21:56:31 -0500

    Delete js directory

--- js/general.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('btn_menu_mobile').addEventListener('click', function () {
-        document.getElementById('nav').className = document.getElementById('nav').className === 'nav hide-on-medium' ? 'nav' : 'nav hide-on-medium';
-    });
-});
